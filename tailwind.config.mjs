7f5e71885d6e09902c80ec914aff9d2b4d7b858e
Sajal Sangal <sajalsangal@gmail.com>
2024-12-17 20:35:43 +0530

    tailwind.config.js -> tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import noScrollbar from 'tailwindcss-no-scrollbar';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    noScrollbar
+  ],
+};
